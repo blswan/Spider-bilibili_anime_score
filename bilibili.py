f93430235a774c17e7d524271d344049c501beae
Black Swan <1124072357@qq.com>
2019-03-24 01:11:39 +0800

    Update bilibili.py

--- bilibili.py
+++ bilibili.py
@@ -5,20 +5,22 @@ from selenium.webdriver.support.wait import WebDriverWait
 from pyquery import PyQuery as pq
 import pymysql
 import time
-import threading
+from multiprocessing import Pool
+from multiprocessing.dummy import Pool as ThreadPool
+import re
+import settings
 
-'''Settings'''
 base_url = 'https://www.bilibili.com/anime/index/?spm_id_from=333.334.b_7072696d6172795f6d656e75.13#season_version=-1&area=-1&is_finish=-1&copyright=-1&season_status=-1&season_month=-1&pub_date=-1&style_id=-1&order=3&st=1&sort=0&page='
-table = 'bilibili_anime'
-host='localhost'
-user='root'
-password='abc123456'
-port= 3306
-database='spiders'
-start_page = 1
-max_page = 155
-threads = 2
-sleep_time = 1
+table = settings.TABLE
+host = settings.HOST
+user = settings.USER
+password = settings.PASSWORD
+port = settings.PORT
+database = settings.DATABASE
+start_page = settings.START_PAGE
+max_page = settings.END_PAGE
+sleep_time = settings.SLEEP_TIME
+threads = settings.THREADS
 
 def get_index(index_response, db):
 	"""
@@ -43,24 +45,21 @@ def get_index(index_response, db):
 		video_urls[anime_name] = anime_url
 	return video_urls
 
-
 def get_detail(detail_response, anime_name, db):
 	"""
 	:detail_response 动漫播放页面的源码
 	:anime_name 动漫名称
 	:db 已打开的数据库
 	"""
-	time.sleep(sleep_time)
 	detail_html = detail_response
 	detail_doc = pq(detail_html)
 	detail_item = detail_doc('#app #bangumi_media')
-	score = detail_item.find('.bangumi-media-header .rate-wrapper .rate-score').text()
+	score = detail_item.find('.bangumi-media-header .rate-wrapper .rate-score').text() or '暂无评分'
 	detail_data = {
 		'name': anime_name,
 		'score': score
 	}
 	update_to_mysql(detail_data, db)
-
 
 def update_to_mysql(data, db):
 	data_keys = ', '.join(data.keys())
@@ -78,12 +77,14 @@ def open_url(url, located_type, located_element, browser):
 		browser.get(url)
 		wait = WebDriverWait(browser, 10)
 		wait.until(EC.presence_of_element_located((located_type, located_element)))
+		time.sleep(sleep_time)
 	except:
 		None
 	return browser.page_source
 
-
-def run(index_url, sem, page):
+def run(index_url):
+	page = re.findall('page=(.*)', index_url)[0]
+	print(f'开始爬取第{page}页')
 	db = pymysql.connect(host=host, user=user, password=password, port=port, db=database)
 	service_args = []
 	service_args.append('--load-images=no')
@@ -93,22 +94,23 @@ def run(index_url, sem, page):
 	video_urls = get_index(index_page_source, db)
 	for anime_name, anime_url in video_urls.items():
 		detail_page_source = open_url('https://' + anime_url, By.CSS_SELECTOR, '#app .bangumi-media-header', browser)
-		get_detail(detail_page_source, anime_name, db, sleep_time)
+		get_detail(detail_page_source, anime_name, db)
+	page = re.findall('page=(.*)', index_url)[0]
 	print('\n==========================')
 	print(f"page {page} successful !")
 	print('==========================\n')
-	time.sleep(sleep_time)
 	db.close()
-	browser.close()
-	sem.release()
+	browser.quit()
 
-def thread(sem):
-	for page in range(start_page, max_page+1):
-		index_url = base_url + str(page)
-		sem.acquire()
-		t = threading.Thread(target=run, args=(index_url,sem, page))
-		t.start()
+def thread(page_list):
+	pool = ThreadPool(threads)
+	results = pool.map(run, page_list)
+	pool.close()
 
 if __name__ == '__main__':
-	sem = threading.Semaphore(threads)
-	thread(sem)
+	page_list = []
+	for page in range(start_page, max_page+1):
+		index_url = base_url + str(page)
+		page_list.append(index_url)
+	thread(page_list)
+	print('全部完成！')
